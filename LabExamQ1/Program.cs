cbb8ecb91856a85a2e490aff3d2734cc687ee26d
Kunal <gajbhiye85@gmail.com>
2023-07-16 14:58:16 +0530

    Sumited Lab

---
+++ LabExamQ1/Program.cs
@@ -0,0 +1,24 @@
+﻿
+namespace LabExamQ1
+{
+    using System;
+    using System.Linq;
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            string[] daysOfWeek = new string[]
+       {
+            "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"
+       };
+            var daysQuery = from day in daysOfWeek
+                            select day;
+
+            foreach (var day in daysQuery)
+            {
+                Console.WriteLine(day);
+            }
+        }
+    }
+}
---
+++ LabExamQ2/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LabExamQ2.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
