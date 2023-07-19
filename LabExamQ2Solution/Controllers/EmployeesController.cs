d62fac386b168fd91986fcd47ddb8e51fa198b38
Kunal <gajbhiye85@gmail.com>
2023-07-19 11:20:56 +0530

    Second commit

---
+++ LabExamQ2Solution/Controllers/EmployeesController.cs
@@ -0,0 +1,78 @@
+﻿using CookiesExample.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+
+namespace CookiesExample.Controllers
+{
+    public class EmployeesController : Controller
+    {
+        // GET: EmployeesController
+        public ActionResult Index()
+        {
+            List<Employee> lstDummy = new List<Employee>();
+            lstDummy.Add(new Employee { Name = "Kunal", Password = "Kunal@123" });
+            return View(lstDummy);
+            
+        }
+
+        // GET: /Employee/Login
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        // POST: /Employee/Login
+        [HttpPost]
+        public IActionResult Login(Employee model)
+        {
+            if (ModelState.IsValid)
+            {
+                // Logic to authenticate the employee
+                if (IsValidEmployee(model.Name, model.Password))
+                {
+                    // Redirect to employee dashboard or any other page
+                    return RedirectToAction("Dashboard");
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Invalid credentials. Please try again.");
+                }
+            }
+
+            return View(model);
+        }
+
+        private bool IsValidEmployee(object username, string password)
+        {
+            throw new NotImplementedException();
+        }
+        // GET: /Employee/Signup
+        public IActionResult Signup()
+        {
+            return View();
+        }
+
+        // POST: /Employee/Signup
+        [HttpPost]
+        public IActionResult Signup(Employee model)
+        {
+            if (ModelState.IsValid)
+            {
+                // Logic to create a new employee account
+                CreateEmployeeAccount(model);
+
+                // Redirect to login page or any other page
+                return RedirectToAction("Login");
+            }
+
+            return View(model);
+        }
+
+        private void CreateEmployeeAccount(Employee model)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ LabExamQ2Solution/Controllers/StudentsController.cs
@@ -0,0 +1,86 @@
+﻿using CookiesExample.Models;
+using Humanizer;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+
+namespace CookiesExample.Controllers
+{
+    public class StudentsController : Controller
+    {
+        // GET: StudentsController
+        public ActionResult Index()
+        {
+            List<Student> lstDummy = new List<Student>();
+            lstDummy.Add(new Student { Name = "Kunal", Password = "Kunal@123" });
+            return View(lstDummy);
+        }
+
+        // GET: /Employee/Login
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        // POST: /Employee/Login
+        [HttpPost]
+        public IActionResult Login(Student model)
+        {
+            if (ModelState.IsValid)
+            {
+                // Logic to authenticate the employee
+                if (IsValidStudent(model.Name, model.Password))
+                {
+                    // Redirect to employee dashboard or any other page
+                    return RedirectToAction("Dashboard");
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Invalid credentials. Please try again.");
+                }
+            }
+
+            return View(model);
+        }
+
+        private bool IsValidStudent(string name, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        // GET: /Employee/Signup
+        public IActionResult Signup()
+        {
+            return View();
+        }
+
+        // POST: /Employee/Signup
+        [HttpPost]
+        public IActionResult Signup(Student model)
+        {
+            if (ModelState.IsValid)
+            {
+                // Logic to create a new employee account
+                CreateStudentAccount(model);
+
+                // Redirect to login page or any other page
+                return RedirectToAction("Login");
+            }
+
+            return View(model);
+        }
+
+        private void CreateStudentAccount(Student model)
+        {
+            throw new NotImplementedException();
+        }
+
+        // Additional actions, such as Dashboard, Edit, Delete, etc., can be added here
+    }
+    
+
+
+
+  }
+
